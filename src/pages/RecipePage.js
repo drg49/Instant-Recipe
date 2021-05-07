23f836fbd83d641e512be1352ba834480c52b51e
Danny Gavin <homevpn14@gmail.com>
2021-05-06 19:42:46 -0700

    Adding the recipes page

--- src/pages/RecipePage.js
+++ src/pages/RecipePage.js
@@ -19,6 +19,31 @@ const RecipePage = (props) => {
         return (
             <div>
                 <h2>{recipe.strMeal}</h2>
+                <img id="recipethumb" src={recipe.strMealThumb} alt={recipe.strMeal} />
+                <p id="origin">Cuisine: {recipe.strArea}</p>
+                <h4>Ingredients</h4>
+                <ul>
+                    <li>{recipe.strIngredient1}</li>
+                    <li>{recipe.strIngredient2}</li>
+                    <li>{recipe.strIngredient3}</li>
+                    <li>{recipe.strIngredient4}</li>
+                    <li>{recipe.strIngredient5}</li>
+                    <li>{recipe.strIngredient6}</li>
+                    <li>{recipe.strIngredient8}</li>
+                    <li>{recipe.strIngredient9}</li>
+                    <li>{recipe.strIngredient10}</li>
+                    <li>{recipe.strIngredient11}</li>
+                    <li>{recipe.strIngredient12}</li>
+                    <li>{recipe.strIngredient13}</li>
+                    <li>{recipe.strIngredient14}</li>
+                    <li>{recipe.strIngredient15}</li>
+                    <li>{recipe.strIngredient16}</li>
+                    <li>{recipe.strIngredient17}</li>
+                    <li>{recipe.strIngredient18}</li>
+                    <li>{recipe.strIngredient19}</li>
+                    <li>{recipe.strIngredient20}</li>
+                </ul>
+            
             </div>
         )
     }
